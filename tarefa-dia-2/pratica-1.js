9da046f1894828851cf5c9466f2a8ddbdb748b08
antonmax29 <antssilva1992@gmail.com>
2022-08-24 23:28:50 -0300

    terceiro commit

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,7 @@
+let idade = 30
+let nome = "Antonio Max"
+let profissao = "auxiliar administrativo | designer | suporte TI"
+let email = "antssilva1992@gmail.com"
+let sonho = "Viajar para ilhas Phi Phi na Tailândia"
+
+console.log(`Meu nome é ${nome}. Tenho ${idade} anos. Minha profissão é ${profissao}. Meu sonho é ${sonho}. Meu email para contato: ${email}.`)
